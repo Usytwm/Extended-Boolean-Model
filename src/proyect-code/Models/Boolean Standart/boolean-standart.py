2540a0ea263bfb0107272fc334f4483d7921d3e0
Eric <eric.tornas@gmail.com>
2024-03-02 22:23:49 -0600

    boolean Standart v0.1

--- src/proyect-code/Models/Boolean Standart/boolean-standart.py
+++ src/proyect-code/Models/Boolean Standart/boolean-standart.py
@@ -1,64 +1,54 @@
-class Boolean_model:
+import gensim
+import spacy
+from sympy import sympify, to_dnf, Not, And, Or
 
-    def __init__(self, documents):
-        self.documents=documents
-        self.tokens_list = {}
+def query_to_dnf(query):
+    processed_query = "A AND (B OR NOT C)"
+    override_and = ('and','AND','&&','&')
+    override_or = ('or','OR','||', '|')
+    override_not = ('not','NOT','~')
 
-    def load_documents(self, documents)-> dict:
-        tokens_list = {}
-
-        for doc in documents:
-            tokens = set(doc.text)
-            for token in tokens:
-                if not tokens_list.__contains__(token):
-                    tokens_list[token] = [doc]
-                else:
-                    tokens_list[token].append(doc)
-
-        print(tokens_list)
-
-        return tokens_list
-
-
-    def load_query(self, query):
-        result = []
-        for i, token in enumerate(query):
-            result.append(token)
-            if i == len(query) - 1:
-                continue
-            if not(query[i] in ("and", "or", "not")) and not query[i+1] in ("and", "or", "not"):
-                result.append("and")
-
-        return result
-
-    def similitud(self, query, token_list):
-        print("Empieza el metodo")
-        print(query)
-        print(token_list)
-        if query[0] == "not":
-            result=set(self.documents).difference(set(token_list[query[1]]))
-        else:
-            result=set(token_list[query[0]])
+    processed_query = [token for token in processed_query.split(' ')]
+    newFND = " "
+    for (i, item) in enumerate(processed_query):
+        if item in override_and:
+            processed_query[i] = override_and[-1]
+            newFND += " &"
+        elif item in override_or:
+            processed_query[i] = override_and[-1]
+            newFND += " |"
+        elif item in override_not:
+            processed_query[i] = override_not[-1]
+            newFND += " ~"
         
-        print(result)
-        for i, token in enumerate(query): # Procesando or
-            if token == "or":
-                if query[i+1] == "not":
-                    result=result.union((set(self.documents)).difference(set(token_list[query[i+2]])))
-                else:
-                    result=result.union(set(token_list[query[i+1]]))
-            elif token == "and":
-                if query[i+1] == "not":
-                    result=result.intersection(set(self.documents).difference(set(token_list[query[i+2]])))
-                else:
-                    result=result.intersection(set(token_list[query[i+1]]))
-            print(result)
+        else:
+            newFND+= " " + processed_query[i]
+        if i < len(processed_query)-1 and (not processed_query[i+1] in override_and) and (not processed_query[i+1] in override_or) and (not processed_query[i+1] in override_not) and not "(":
+            newFND+=" &"  
+    return newFND
 
-        print("Resultado")
-        print(result)
-        return result
+    # Convertir a expresión sympy y aplicar to_dnf
+    query_expr = sympify(processed_query, evaluate=False)
+    query_dnf = to_dnf(query_expr, simplify=True)
 
+    return query_dnf
 
+def get_matching_docs(query_dnf,tokenized_docs):
+  
 
+  # Convertir tokenized_docs a un conjunto de sets
+  doc_term_sets = [set(doc) for doc in tokenized_docs]
 
+  matching_documents = []
+  for doc_i, doc_terms in enumerate(doc_term_sets):
+    # Short-circuiting: si no hay coincidencia con una componente conjuntiva, se ignora el documento
+    all_match = True
+    for q_ce in query_dnf:
+      if not q_ce.issubset(doc_terms):
+        all_match = False
+        break
+    if all_match:
+      matching_documents.append(doc_i)
+
+  return matching_documents
 
