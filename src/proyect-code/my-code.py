4d8268aa1d16b69e75c9e6528905454485191e10
Brian <gisameht@gmail.com>
2024-03-05 11:14:00 -0500

    dats

--- src/proyect-code/my-code.py
+++ src/proyect-code/my-code.py
@@ -5,8 +5,19 @@ from gensim.corpora import Dictionary
 import json
 from Models.Boolean_Extended_Model.boolean_extended_model import ExtendedBooleanModel
 
-dataset = ir_datasets.load("beir/arguana")
+dataset = ir_datasets.load("cranfield")
 terminos_consulta = "vegetarian animal culito"
+dictionary = {}
+for query_id, doc_id, relevance, iteration in dataset.qrels_iter():
+    if query_id not in dictionary:
+        dictionary[query_id] = [doc_id]
+    else:
+        dictionary[query_id].append(doc_id)
+
+# for query_id, doc_ids in dictionary.items():
+#     if len(doc_ids) > 2:
+#         print(query_id)
+# print(dictionary)
 
 
 def relevant_documents(query_id: str):
@@ -68,3 +79,6 @@ def recovered_documents_sri(query):
     relevance_scores = boolean_extended_model.all_documents_relevance_and()
 
     return enumerate(relevance_scores)
+
+
+print(recovered_documents_sri(terminos_consulta))
